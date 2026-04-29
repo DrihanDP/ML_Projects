f1ce0d3bddd4cb66990e7a672f4bf6512b42a66c
Drihan du Preez <drihan@btinternet.com>
2026-04-29 17:52:53 +0100

    added more libraries and completed initial linear regression

--- Linear Regression Simple/HousingPricePrediction/main.py
+++ Linear Regression Simple/HousingPricePrediction/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from housingpriceprediction!")
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
+data = pd.read_csv("data.csv")
+data = data[["date", "price", "bedrooms", "bathrooms", "sqft_living", "sqft_lot", "floors", "waterfront", "view", 
+             "sqft_above", "sqft_basement", "yr_built", "yr_renovated", "street", "city", "statezip", "country"]]
 
-if __name__ == "__main__":
-    main()
